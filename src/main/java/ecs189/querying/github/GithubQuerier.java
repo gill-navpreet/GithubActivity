1c239bdf317b912dcdf27a7a052a2ded79e21d5e
gill-navpreet <nkgkaur@ucdavis.edu>
2017-12-03 11:07:14 -0800

    Shows Push events

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -23,6 +23,7 @@ public class GithubQuerier {
         List<JSONObject> response = getEvents(user);
         StringBuilder sb = new StringBuilder();
         sb.append("<div>");
+
         for (int i = 0; i < response.size(); i++) {
             JSONObject event = response.get(i);
             // Get event type
@@ -55,12 +56,39 @@ public class GithubQuerier {
     private static List<JSONObject> getEvents(String user) throws IOException {
         List<JSONObject> eventList = new ArrayList<JSONObject>();
         String url = BASE_URL + user + "/events";
-        System.out.println(url);
-        JSONObject json = Util.queryAPI(new URL(url));
-        System.out.println(json);
-        JSONArray events = json.getJSONArray("root");
-        for (int i = 0; i < events.length() && i < 10; i++) {
-            eventList.add(events.getJSONObject(i));
+        int page = 1;
+        int counter = 0;
+        boolean valid = true;
+
+        while(valid){
+            System.out.println(url);
+            JSONObject json = Util.queryAPI(new URL(url+ "?page=" + page));
+            System.out.println(json);
+            JSONArray events = json.getJSONArray("root");
+
+            int i=0;
+            int loopIteration = 0;
+            loopIteration = events.length(); // get the number of total events
+
+//            for (int i = 0; i < events.length() && i < 10; i++) {
+//                eventList.add(events.getJSONObject(i));
+//            }
+            while (loopIteration > 0 && counter < 10){
+                JSONObject users = events.getJSONObject(i);
+                String strType = users.getString("type");
+
+                if (strType.equals("PushEvent")){
+                    eventList.add(events.getJSONObject(i));
+                    counter++;
+                    if (counter >= 10){
+                        valid = false;
+                        break;
+                    }
+                }
+                loopIteration--;
+                i++;
+            }
+            page++;
         }
         return eventList;
     }
